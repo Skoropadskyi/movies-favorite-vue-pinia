3aec22242f6e1e7e98102358866305cfc6efb077
Oleg <skorpik20@gmail.com>
2024-11-27 16:54:21 +0200

    composition API

--- src/stores/searchStore.js
+++ src/stores/searchStore.js
@@ -5,23 +5,28 @@ import { ref } from "vue";
 const url =
     "https:api.themoviedb.org/3/search/movie?api_key=43ec5ecf5e49546660cf625c7bfa329b&query=";
 
-export const useSearchStore = defineStore("searchStore", {
-    state: () => ({
-        loader: false,
-        movies: [],
-    }),
-    actions: {
-        async getMovies(search) {
-            this.loader = true;
-            const res = await fetch(`${url}${search}`);
-            const data = await res.json();
-            this.movies = data.results;
-            this.loader = false;
-        },
-        addToUserMovies(object) {
-            const movieStore = useMovieStore();
-            movieStore.movies.push({...object, isWatched: false});
-            movieStore.activeTab = 1;
-        }
-    },
-});
+export const useSearchStore = defineStore("searchStore", () => {
+    const loader = ref(false);
+    const movies = ref([]);
+
+    const getMovies = async(search) => {
+        loader.value = true;
+        const res = await fetch(`${url}${search}`);
+        const data = await res.json();
+        movies.value = data.results;
+        loader.value = false;
+    }
+
+    const addToUserMovies = (object) => {
+        const movieStore = useMovieStore();
+        movieStore.movies.push({...object, isWatched: false});
+        movieStore.activeTab = 1;
+    }
+
+    return {
+        loader,
+        movies,
+        getMovies,
+        addToUserMovies
+    }
+})
